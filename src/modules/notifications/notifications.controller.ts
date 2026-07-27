eb21f58379911d0d8e53976b0e556d3305d07736
TankistPro <nakol.nayk@mail.ru>
2026-07-27 23:38:01 +0300

    feat: add get and read notifications gRPC endpoints

--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -1,20 +1,45 @@
+import { Controller } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { GrpcMethod } from '@nestjs/microservices';
 import type {
+  GetNotificationsRequest,
+  GetNotificationsResponse,
+  ReadNotificationRequest,
+  ReadNotificationResponse,
   SendNotificationRequest,
   SendNotificationResponse,
 } from '@voice-chat/contracts/gen/notification';
 
+@Controller()
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
-  @GrpcMethod('NotificationsService', 'SendNotification')
-  async sendNotification(
+  @GrpcMethod('NotificationService', 'SendNotification')
+  async SendNotification(
     request: SendNotificationRequest,
   ): Promise<SendNotificationResponse> {
     const notification =
       await this.notificationsService.sendNotification(request);
 
     return { notification };
+  }
+
+  @GrpcMethod('NotificationService', 'GetNotifications')
+  async getNotifications(
+    request: GetNotificationsRequest,
+  ): Promise<GetNotificationsResponse> {
+    const notifications =
+      await this.notificationsService.getNotifications(request);
+
+    return { notifications };
+  }
+
+  @GrpcMethod('NotificationService', 'ReadNotification')
+  async readNotification(
+    request: ReadNotificationRequest,
+  ): Promise<ReadNotificationResponse> {
+    const status = await this.notificationsService.readNotification(request);
+
+    return { status };
   }
 }
--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { RpcStatus } from '@voice-chat/common';
 import type {
+  GetNotificationsRequest,
   Notification,
+  ReadNotificationRequest,
   SendNotificationRequest,
 } from '@voice-chat/contracts/gen/notification';
 import { NotificationTypes } from 'prisma/generated/enums';
@@ -46,6 +48,59 @@ export class NotificationsService {
       throw new RpcException({
         code: RpcStatus.INTERNAL,
         details: 'Ошибка при отправке уведомление',
+      });
+    }
+  }
+
+  async getNotifications(
+    request: GetNotificationsRequest,
+  ): Promise<Notification[]> {
+    const { receiverId, senderId } = request;
+
+    try {
+      const notifications = await this.prismaClient.notification.findMany({
+        where: {
+          receiverId,
+          senderId,
+        },
+      });
+
+      return notifications.map((n) => ({
+        ...n,
+        createdAt: n.createdAt.toISOString(),
+      }));
+    } catch (error) {
+      console.error(error);
+      throw new RpcException({
+        code: RpcStatus.INTERNAL,
+        details: 'Ошибка при получении уведомлений',
+      });
+    }
+  }
+
+  async readNotification(request: ReadNotificationRequest): Promise<boolean> {
+    const { notificationIds } = request;
+    console.log(request);
+    if (!notificationIds || !notificationIds.length) return false;
+
+    try {
+      const success = await this.prismaClient.notification.updateMany({
+        where: {
+          id: {
+            in: notificationIds,
+          },
+        },
+        data: {
+          isRead: true,
+        },
+      });
+
+      return success.count > 0;
+    } catch (error) {
+      console.error(error);
+      throw new RpcException({
+        code: RpcStatus.INTERNAL,
+        details: 'Не удалось прочитать уведомление',
       });
     }
   }
